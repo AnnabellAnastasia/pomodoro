4caff52855287882713b00f935dab2aa7e1b4b43
Annabella <oav2008@gmail.com>
2024-07-30 09:18:55 -0400

    adding label and buttons

--- main.py
+++ main.py
@@ -19,13 +19,30 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
-canvas = Canvas(width=200, height=224)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Label
+label = Label(text="Timer", bg=YELLOW, fg=GREEN, pady=10, padx=10, font=(FONT_NAME, 50))
+label.grid(row=0, column=1)
+
+# Button
+button_start = Button(text="Start", highlightthickness=0, bg=YELLOW)
+button_start.grid(row=2, column=0)
+
+
+button_reset = Button(text="Reset", highlightthickness=0, bg=YELLOW)
+button_reset.grid(row=2, column=2)
+
+label_check_mark = Label(text="✓", bg=YELLOW, fg=GREEN )
+label_check_mark.grid(row=3, column=1)
 
 
 window.mainloop()
